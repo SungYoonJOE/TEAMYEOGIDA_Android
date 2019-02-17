c85b150caaeaf3f6ed49d9e53f22f4bcf1dedfde
sungyoonJOE <hahajsu@gmail.com>
2019-02-17 16:23:19 +0900

    Main통신 - 지역이 전체일 때 서버로부터 상품정보 최신순, 인기순, 마감순 리스트에 받기(f1, f2, f3)

--- yeogida8/app/src/main/java/com/example/hahaj/yeogida8/Fragment1.java
+++ yeogida8/app/src/main/java/com/example/hahaj/yeogida8/Fragment1.java
@@ -3,6 +3,7 @@ package com.example.hahaj.yeogida8;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -17,6 +18,20 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 //최신순
@@ -27,48 +42,38 @@ public class Fragment1 extends Fragment {
     EditText editText;
     EditText editText2;
     int ppid=0;
-    String f1area;
+    ListView listView;
+    private NetworkUrl url = new NetworkUrl();
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        Intent f1intent = getActivity().getIntent();
-        //personpid = f1intent.getIntExtra("personpid", 0);
-        f1area = f1intent.getStringExtra("area");
-        //Log.d("f1 최신순 ppid : ", ""+personpid);
-        //Log.d("f1 area : ", f1area);
+/*
+        //충격적인 사실 : Activity에서 Fragment로 데이터를 전달할 때는 Intent가 아니라 Bundle을 사용한다.
+        Bundle bundleF1 = getArguments();
+        if(bundleF1 == null){
+            Log.d("bundle이 null일 경우", "null입니다..");
+        }
+        String area = bundleF1.getString("areaFromMain");
+        Log.d("메인에서 온 지역", area);
+*/
 
         //personpid 불러오기
         SharedPreferences pref = this.getActivity().getSharedPreferences("pref_PERSONPID", Context.MODE_PRIVATE);
-        //pref_PERSONPID파일의 personpid 키에 있는 데이터를 가져옴. 없으면 0을 리턴
-        ppid = pref.getInt("personpid", 0);
+        ppid = pref.getInt("personpid", 0);//pref_PERSONPID파일의 personpid 키에 있는 데이터를 가져옴. 없으면 0을 리턴
         Log.d("ppid in 최신순목록>> ", "" + ppid);
 
+        //다른 화면 -> 메인으로 오자마자 default값(지역: 전체) 상품들을 받는 통신코드
+        new DefaultMainJSONTask().execute(url.getMainUrl()+"?"+"area"+"="+"");
+
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment1, container, false);
-        ListView listView = (ListView) rootView.findViewById(R.id.listView);
-
-        final HotelAdapter adapter=new HotelAdapter();
-        adapter.addItem(new HotelItem(1, "http://cfs11.tistory.com/image/33/tistory/2009/02/26/22/41/49a69bf854e7c", "대영호텔", "2019-01-01", "2019-01-02", "서울시 광진구 자양동", 500,100));
-        adapter.addItem(new HotelItem(2, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRJe8n7J51TfYyKlP2SK-72TisT2P2t8tPGsVxPtqqhvuCBCQeZw", "진권호텔", "2019-01-02", "2019-01-03", "서울시 광진구 자양동", 600,200));
-        adapter.addItem(new HotelItem(3, "http://cfs11.tistory.com/image/33/tistory/2009/02/26/22/41/49a69bf854e7c", "성준호텔", "2019-01-03", "2019-01-04", "서울시 광진구 자양동", 700,300));
-        adapter.addItem(new HotelItem(4, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRJe8n7J51TfYyKlP2SK-72TisT2P2t8tPGsVxPtqqhvuCBCQeZw", "지해호텔", "2019-01-04", "2019-01-05", "서울시 광진구 자양동", 800,400));
-        adapter.addItem(new HotelItem(5, "http://cfs11.tistory.com/image/33/tistory/2009/02/26/22/41/49a69bf854e7c", "유림호텔", "2019-01-05", "2019-01-06", "서울시 광진구 자양동", 900,500));
-        adapter.addItem(new HotelItem(6, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRJe8n7J51TfYyKlP2SK-72TisT2P2t8tPGsVxPtqqhvuCBCQeZw", "성윤호텔", "2019-01-06", "2019-01-07", "서울시 광진구 자양동", 1000,600));
-        adapter.addItem(new HotelItem(7, "http://cfs11.tistory.com/image/33/tistory/2009/02/26/22/41/49a69bf854e7c", "상명호텔", "2019-01-07", "2019-01-08", "서울시 광진구 자양동", 1100,700));
-
-        //adapter.addItem(new HotelItem("박성준","상명호텔","경기 고양","2000","3000","20181201","20181231",R.drawable.ic_hotel));
-        //adapter.addItem(new HotelItem("박민수","상명호텔","경기 고양","2000","3000","20181201","20181231",R.drawable.ic_hotel2));
-
-        listView.setAdapter(adapter);
+        listView = (ListView) rootView.findViewById(R.id.listView);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) //position은 몇번째 아이템인지 인덱스값
             {
-                HotelItem item = (HotelItem) adapter.getItem(position);
-                Toast.makeText(getContext(),"선택 : "+item.getPname(), Toast.LENGTH_LONG).show();
-                //서버에 personpid를 전송
 
                 //인텐트사용하여 상품정보(상품구매)화면으로 이동
                 redirectMainToBuyActivity(ppid);
@@ -80,38 +85,40 @@ public class Fragment1 extends Fragment {
 
 
     //어댑터를 이용하여 data관리, 어댑터의 리턴값이 아이템으로 보임
-
     class HotelAdapter extends BaseAdapter {
+        ArrayList<HotelItem> items = new ArrayList<HotelItem>();
 
-        ArrayList<HotelItem> items=new ArrayList<HotelItem>();
+        @Override
+        public int getCount() //몇개의 아이템이 있니?
+        {
+            return items.size();
+        }
 
         public void addItem(HotelItem item) {
             items.add(item);
         }
 
         @Override
-        public int getCount() {
-            return items.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
+        public Object getItem(int position) //몇 번째 아이템인지
+        {
             return items.get(position);
         }
 
         @Override
-        public long getItemId(int position) {
+        public long getItemId(int position) //id있음 넘겨줘
+        {
             return position;
         }
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
+        public View getView(int position, View convertView, ViewGroup parent)
+        {
             HotelItemView view=null;
             if(convertView==null) {
-                view=new HotelItemView(getContext());
+                view= new HotelItemView(getContext());
             }
             else{
-                view=(HotelItemView) convertView;
+                view= (HotelItemView) convertView;
             }
             HotelItem item=items.get(position);
             view.setProductname(item.getPname());
@@ -134,4 +141,117 @@ public class Fragment1 extends Fragment {
         startActivityForResult(intent, BasicInfo.REQUEST_CODE_MAINTOBUY);
     }
 
+    //다른 화면 -> 메인으로 오자마자 default값(지역: 전체) 상품들을 받는 통신코드
+    public class DefaultMainJSONTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                Log.d("doInBackground 확인", "doIn");
+                //JSONObject를 만들고 key value 형식으로 값을 저장해준다.
+
+                String area = ""; // 지역(전체)에 해당하는 상품을 받기 위해 서버에 보내는 값
+
+                JSONObject jsonObject = new JSONObject();
+
+                jsonObject.put("area", area);
+                Log.d("area 값>>", "" + area);
+
+                HttpURLConnection con = null;
+                BufferedReader reader = null;
+
+
+                try {
+                    URL url = new URL(urls[0]);
+                    //연결을 함
+                    con = (HttpURLConnection) url.openConnection();
+                    con.setRequestMethod("GET");//GET방식으로 보냄
+                    con.setRequestProperty("Cache-Control", "no-cache");//캐시 설정
+                    con.setRequestProperty("Content-Type", "application/json");//application JSON 형식으로 전송
+                    con.setRequestProperty("Accept", "text/html");//서버에 response 데이터를 html로 받음
+                    con.setDoOutput(false); //true이면 내부적으로 다시 POST로 바꾸기 때문에 GET을 위해 false
+                    con.setDoInput(true);//Inputstream으로 서버로부터 응답을 받겠다는 의미
+                    con.connect();
+
+/*
+                    //서버로 보내기위해서 스트림 만듬
+                    OutputStream outStream = con.getOutputStream();
+                    //버퍼를 생성하고 넣음
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outStream));
+                    writer.write(jsonObject.toString());
+                    writer.flush();
+                    writer.close();//버퍼를 받아줌
+*/
+                    //서버로 부터 데이터를 받음
+                    InputStream stream = con.getInputStream();
+                    reader = new BufferedReader(new InputStreamReader(stream));
+                    StringBuffer buffer = new StringBuffer();
+                    String line = "";
+
+                    while ((line = reader.readLine()) != null) {
+                        buffer.append(line);
+                    }
+
+                    return buffer.toString();//서버로 부터 받은 값을 리턴해줌 아마 OK!!가 들어올것임
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (con != null) {
+                        con.disconnect();
+                    }
+                    try {
+                        if (reader != null) {
+                            reader.close();//버퍼를 닫아줌
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return "";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            final HotelAdapter adapter=new HotelAdapter();
+            if(result==null) return;
+
+            try {
+                JSONArray jsonArray = new JSONArray(result);
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    int productpid = jsonObject.getInt("productpid");
+                    String productname = jsonObject.getString("productname");
+                    int forprice = jsonObject.getInt("formerprice");
+                    int productprice = jsonObject.getInt("productprice");
+                    String date_s = jsonObject.get("productdate_s").toString().substring(0, 10);
+                    String date_e = jsonObject.get("productdate_e").toString().substring(0, 10);
+                    String productimg = jsonObject.getString("productimage");
+                    String productaddr = jsonObject.getString("productaddress");
+                    int producthit = jsonObject.getInt("producthit");
+
+                    Log.d("메인리스트1", "상품pid" + productpid + ", ," + productimg + ", 상품이름," + productname);
+                    Log.d("메인리스트2", "원가: " + forprice + ", 판매가: " + productprice);
+                    Log.d("메인리스트3", "시작" + date_s + "끝" + date_e );
+                    Log.d("메인리스트4", "상품 이미지: " + productimg);
+                    Log.d("메인리스트4", "상품 주소:"+productaddr+", 히트 수: "+producthit);
+
+                    adapter.addItem(new HotelItem(productpid, productimg, productname, date_s, date_e, productaddr, forprice, productprice));
+                    listView.setAdapter(adapter);
+                }
+
+            }catch (JSONException e1){
+                e1.printStackTrace();
+            }
+        }
+    }
 }
--- yeogida8/app/src/main/java/com/example/hahaj/yeogida8/NetworkUrl.java
+++ yeogida8/app/src/main/java/com/example/hahaj/yeogida8/NetworkUrl.java
@@ -4,7 +4,7 @@ public class NetworkUrl {
 
     //main URL
     //Url 바뀌면 여기만 바꾸면 됨.
-    private String main = new String("http://06436ed5.ngrok.io");
+    private String main = new String("http://d23286fe.ngrok.io");
 
     public String getMainUrl() {
         return this.main;
